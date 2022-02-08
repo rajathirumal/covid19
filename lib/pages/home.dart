da2ef30aa9e037744e02d895827f82c0ddfc5b83
Raja Thirumal Govindaraj <rajathirumal98@gmail.com>
2022-02-08 22:46:13 +0530

    implimentation of adding more horizontal tile feature - on progress

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../source/data.source.dart';
@@ -16,9 +18,9 @@ class HomePage extends StatefulWidget {
 ///   2.a Show world data - https://disease.sh/v3/covid-19/all - Done (Yet to implimented API call)
 ///   2.c Show only selected fields from API responce - Done
 ///   2.d Let the user add the required fields to the horizontal scroll - (Yet to give UI to pic fields)
-/// 3. Have a refresh icon above the top 10 list to refresh data in it
+/// 3. Have a refresh icon above the top 10 list to refresh data in it - (Icon done yet to impliment functionality)
 /// 4. Show top 10 Country Data  - https://disease.sh/v3/covid-19/countries?sort=cases
-///   4.a Let user pick ( cases, todayCases, deaths, recovered, active )
+///   4.a Let user pick either of cases, todayCases, deaths, recovered, active - (UI done, yet to impliment functionality)
 ///   4.c When the user picks the sort criteria don't make API call, sort the local data - use below snippet
 ///       top10Countries.sort((a, b) => (b["countryInfo"]["_id"]).compareTo(a["countryInfo"]["_id"]));
 ///
@@ -64,8 +66,10 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> myData = MocData.top10Countries;
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Advisory // Task 1
           Container(
@@ -81,6 +85,81 @@ class _HomePageState extends State<HomePage> {
                   fontSize: 16),
             ),
           ),
+          // Show:
+          // Text   = "All over the world"
+          // Button = Add
+
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 10.0, top: 5, bottom: 0, right: 10.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "All over the world",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 61, 58, 58),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => {
+                    showDialog<String>(
+                      context: context,
+                      builder: (context) => SimpleDialog(
+                        title: const Text("Please select"),
+                        children: <Widget>[
+                          ListTile(
+                              title: const Text("Total cases"),
+                              onTap: () => Navigator.pop(context, "cases")),
+                          ListTile(
+                              title: const Text("Total Deths"),
+                              onTap: () => Navigator.pop(context, "deaths")),
+                          ListTile(
+                              title: const Text("Active cases"),
+                              onTap: () => Navigator.pop(context, "active")),
+                        ],
+                      ),
+                    ).then(
+                      (selectedOption) {
+                        if (selectedOption != null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("you have clicked $selectedOption"),
+                              action: SnackBarAction(
+                                label: "OK",
+                                onPressed: () {},
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  },
+                  child: const Icon(
+                    Icons.add,
+                    color: Color.fromARGB(255, 61, 58, 58),
+                    size: 35.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Container(
+          //   margin: const EdgeInsets.only(left: 10.0, top: 5, bottom: 0),
+          //   child: const Text(
+          //     "All over the world",
+          //     style: TextStyle(
+          //       color: Color.fromARGB(255, 61, 58, 58),
+          //       fontSize: 20,
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
+
           // Horizontal scrolling List for task 2
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -140,9 +219,7 @@ class _HomePageState extends State<HomePage> {
                         size: 30.0,
                       ),
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
+                    const SizedBox(width: 10),
                     GestureDetector(
                       onTap: () {
                         showDialog<String>(
@@ -152,23 +229,32 @@ class _HomePageState extends State<HomePage> {
                             children: <Widget>[
                               ListTile(
                                   title: const Text("Total cases"),
-                                  onTap: () =>
-                                      Navigator.pop(context, "Total cases")),
+                                  onTap: () => Navigator.pop(context, "cases")),
                               ListTile(
                                   title: const Text("Total Deths"),
                                   onTap: () =>
-                                      Navigator.pop(context, "Deaths")),
+                                      Navigator.pop(context, "deaths")),
                               ListTile(
                                   title: const Text("Active cases"),
                                   onTap: () =>
-                                      Navigator.pop(context, "Active cases")),
+                                      Navigator.pop(context, "active")),
                             ],
                           ),
                         ).then(
                           (selectedOption) {
+                            // print(myData);
                             if (selectedOption != null) {
                               setState(
-                                  () => sortCountryListBy = selectedOption);
+                                () {
+                                  sortCountryListBy = selectedOption;
+
+                                  // myData.sort((a, b) => (b["countryInfo"]
+                                  //         ["selectedOption"])
+                                  //     .compareTo(
+                                  //         a["countryInfo"]["selectedOption"]));
+                                },
+                              );
+
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
                                   content:
@@ -191,11 +277,11 @@ class _HomePageState extends State<HomePage> {
                             size: 30.0,
                           ),
                           Text(
-                            sortCountryListBy,
+                            sortCountryListBy[0].toUpperCase() +
+                                sortCountryListBy.substring(1).toLowerCase(),
                             style: const TextStyle(
                               color: Color.fromARGB(255, 61, 58, 58),
                               fontSize: 15,
-                              fontStyle: FontStyle.italic,
                             ),
                           ),
                         ],
@@ -207,10 +293,12 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           // Top 10 countries ListView
+          // Shows 10 data form `myData`
           Expanded(
             child: ListView.builder(
               itemCount: 10,
-              itemBuilder: (context, index) => top10CountriesCard(index),
+              itemBuilder: (context, index) =>
+                  top10CountriesCard(index, myData),
             ),
           ),
         ],
@@ -218,117 +306,68 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget top10CountriesCard(int index) {
-    return ListTile(
-      leading: Card(
-        elevation: 25,
-        child: Image.network(
-          MocData.top10Countries[index]["countryInfo"]["flag"],
-          height: 50,
-          width: 60,
-          fit: BoxFit.fill,
-          loadingBuilder: (context, child, progress) {
-            return progress == null
-                ? child
-                : const CircularProgressIndicator.adaptive();
-          },
-          semanticLabel: 'Flag of ${MocData.top10Countries[index]["country"]}',
+  Widget top10CountriesCard(int index, List<Map<String, dynamic>> countryData) {
+    return Card(
+      elevation: 10,
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+      child: ListTile(
+        onTap: () {
+          // Call a new Country Page with -> countryData[index]
+          // If call is from home.dart, show country details with the details passed from this
+          // Else If call is from drawer.dart, show option for the user to select country name
+        },
+        leading: Card(
+          elevation: 25,
+          child: Image.network(
+            countryData[index]["countryInfo"]["flag"],
+            height: 50,
+            width: 60,
+            fit: BoxFit.fill,
+            loadingBuilder: (context, child, progress) {
+              return progress == null
+                  ? child
+                  : const CircularProgressIndicator.adaptive();
+            },
+            semanticLabel: 'Flag of ${countryData[index]["country"]}',
+          ),
         ),
-      ),
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('COUNTRY : ${MocData.top10Countries[index]["country"]}'),
-          Text('CASES        : ${MocData.top10Countries[index]["cases"]}'),
-          Text('DEATHS      : ${MocData.top10Countries[index]["deaths"]}'),
-          Text('ACTIVE CASES : ${MocData.top10Countries[index]["active"]}'),
-        ],
-      ),
-      trailing: Column(
-        children: [
-          Text(MocData.top10Countries[index]["countryInfo"]["iso3"]),
-          const Icon(
-            Icons.arrow_forward,
-            color: Color.fromARGB(255, 61, 58, 58),
-            size: 30.0,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget xtop10CountriesCard(int index) {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      child: Card(
-        color: Colors.white,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
+        title: Row(
           children: [
-            /// Column:
-            /// 1. Flag
-            /// 2. Country code
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 0.05,
-                      ),
-                    ),
-                    child: Image.network(
-                      MocData.top10Countries[index]["countryInfo"]["flag"],
-                      height: 50,
-                      width: 70,
-                      fit: BoxFit.fill,
-                      loadingBuilder: (context, child, progress) {
-                        return progress == null
-                            ? child
-                            : const CircularProgressIndicator.adaptive();
-                      },
-                      semanticLabel:
-                          'Flag of ${MocData.top10Countries[index]["country"]}',
-                    ),
-                  ),
-                  Text(MocData.top10Countries[index]["countryInfo"]["iso3"]),
-                ],
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('COUNTRY'),
+                Text('CASES '),
+                Text('DEATHS '),
+                Text('ACTIVE CASES'),
+              ],
             ),
-
-            /// Column:
-            /// Country data
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('COUNTRY : ${MocData.top10Countries[index]["country"]}'),
-                  Text(
-                      'CASES        : ${MocData.top10Countries[index]["cases"]}'),
-                  Text(
-                      'DEATHS      : ${MocData.top10Countries[index]["deaths"]}'),
-                  Text(
-                      'ACTIVE CASES : ${MocData.top10Countries[index]["active"]}'),
-                ],
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(': ${countryData[index]["country"]}',
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                Text(': ${countryData[index]["cases"]}'),
+                Text(': ${countryData[index]["deaths"]}'),
+                Text(': ${countryData[index]["active"]}'),
+              ],
             ),
-
-            /// Column:
-            /// Show more data icon
-            // Column(
-            //   crossAxisAlignment: CrossAxisAlignment.end,
-            //   children: const [
-            // Icon(
-            //   Icons.refresh_outlined,
-            //   color: Color.fromARGB(255, 61, 58, 58),
-            //   size: 30.0,
-            // ),
-            //   ],
-            // )
+          ],
+        ),
+        trailing: Column(
+          children: [
+            const Icon(
+              Icons.arrow_forward,
+              color: Color.fromARGB(255, 61, 58, 58),
+              size: 30.0,
+            ),
+            Text(
+              countryData[index]["countryInfo"]["iso3"],
+              style: const TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0),
+            ),
           ],
         ),
       ),
